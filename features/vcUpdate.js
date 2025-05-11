4d6b073a80048eb0f070001dcee6de80ad803d59
Tanishq Mehrunkar <mehrunkart@gmail.com>
2025-05-12 00:27:25 +0530

    update music and feature

--- features/vcUpdate.js
+++ features/vcUpdate.js
@@ -9,6 +9,44 @@ const vcUpdate=()=>
         const ch=client.channels.cache.get("962590186598989824");
         if(oldState.channelId===null) ch.send(`VC se baahar nikal laude ${userMention(newState.id)}`);
         else if(newState.channelId===null) ch.send(`VC me vaapis aa laude ${userMention(oldState.id)}`);
+        else if(oldState.selfDeaf !== newState.selfDeaf)
+        {
+            if(newState.selfDeaf) ch.send(`Deaf ho gaya ${userMention(newState.id)} laude`);
+            else ch.send(`Undeaf ho gaya ${userMention(newState.id)} laude`);
+        }
+        else if(oldState.selfMute !== newState.selfMute)
+        {
+            if(newState.selfMute)
+            {
+                ch.send(`Le BSDK ${userMention(newState.id)} bin mic wale`);
+                const role=newState.guild.roles.cache.find(role => role.name === "Bin Mic Wale");
+                if(role)
+                {
+                    const member=newState.guild.members.cache.get(newState.id);
+                    if(member)
+                    {
+                        member.roles.add(role).catch(console.error);
+                    }
+                    else ch.send(`Member nahi mila ${userMention(newState.id)} laude`);
+                }
+                else ch.send(`Role nahi mila ${userMention(newState.id)} laude`);
+            }
+            else
+            {
+                ch.send(`Le BSDK ${userMention(newState.id)} hata diya bin mic wale`);
+                const role=newState.guild.roles.cache.find(role => role.name === "Bin Mic Wale");
+                if(role)
+                {
+                    const member=newState.guild.members.cache.get(newState.id);
+                    if(member)
+                    {
+                        member.roles.remove(role).catch(console.error);
+                    }
+                    else ch.send(`Member nahi mila ${userMention(newState.id)} laude`);
+                }
+                else ch.send(`Role nahi mila ${userMention(newState.id)} laude`);
+            }
+        }
     });
 }
 module.exports=vcUpdate;
