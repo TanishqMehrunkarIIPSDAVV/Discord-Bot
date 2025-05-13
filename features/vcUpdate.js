facfe48af6311159eb11e3950d33f9342f0fa816
Tanishq Mehrunkar <mehrunkart@gmail.com>
2025-05-13 22:23:00 +0530

    vc update

--- features/vcUpdate.js
+++ features/vcUpdate.js
@@ -1,52 +1,44 @@
-const path=require("node:path");
-const client=require(`${path.dirname(__dirname)}/index.js`);
-const {userMention}=require("discord.js");
+const path = require("node:path");
+const client = require(`${path.dirname(__dirname)}/index.js`);
+const { userMention } = require("discord.js");
+const {SpeechClient} = require("discord-speech-recognition");
 
-const vcUpdate=()=>
-{
-    client.on('voiceStateUpdate', (oldState, newState) =>
-    {
-        const ch=client.channels.cache.get("962590186598989824");
-        if(oldState.channelId===null) ch.send(`VC se baahar nikal laude ${userMention(newState.id)}`);
-        else if(newState.channelId===null) ch.send(`VC me vaapis aa laude ${userMention(oldState.id)}`);
-        else if(oldState.selfDeaf !== newState.selfDeaf)
-        {
-            if(newState.selfDeaf) ch.send(`Deafen ho gaya ${userMention(newState.id)} laude`);
-            else ch.send(`Undeafen ho gaya ${userMention(newState.id)} laude`);
-        }
-        else if(oldState.selfMute !== newState.selfMute)
-        {
-            if(newState.selfMute)
-            {
-                ch.send(`Le BSDK ${userMention(newState.id)} bin mic wale`);
-                const role=newState.guild.roles.cache.find(role => role.name === "Bin Mic Wale");
-                if(role)
-                {
-                    const member=newState.guild.members.cache.get(newState.id);
-                    if(member)
-                    {
-                        member.roles.add(role).catch(console.error);
-                    }
-                    else ch.send(`Member nahi mila ${userMention(newState.id)} laude`);
-                }
-                else ch.send(`Role nahi mila ${userMention(newState.id)} laude`);
-            }
-            else
-            {
-                ch.send(`Le BSDK ${userMention(newState.id)} hata diya bin mic wale`);
-                const role=newState.guild.roles.cache.find(role => role.name === "Bin Mic Wale");
-                if(role)
-                {
-                    const member=newState.guild.members.cache.get(newState.id);
-                    if(member)
-                    {
-                        member.roles.remove(role).catch(console.error);
-                    }
-                    else ch.send(`Member nahi mila ${userMention(newState.id)} laude`);
-                }
-                else ch.send(`Role nahi mila ${userMention(newState.id)} laude`);
-            }
-        }
-    });
-}
-module.exports=vcUpdate;
+const roleAssign = (newState) => {
+  if (newState.selfMute) {
+    const role = newState.guild.roles.cache.find(
+      (role) => role.name === "Bin Mic Wale"
+    );
+    if (role) {
+      const member = newState.guild.members.cache.get(newState.id);
+      if (member) {
+        member.roles.add(role).catch(console.error);
+      }
+    }
+  } else {
+    const role = newState.guild.roles.cache.find(
+      (role) => role.name === "Bin Mic Wale"
+    );
+    if (role) {
+      const member = newState.guild.members.cache.get(newState.id);
+      if (member) {
+        member.roles.remove(role).catch(console.error);
+      }
+    }
+  }
+};
+
+const vcUpdate = () => {
+  client.on("voiceStateUpdate", (oldState, newState) => {
+    const ch = client.channels.cache.get("962590186598989824");
+    if (oldState.channelId === null) {
+      ch.send(`${userMention(newState.id)} joined the VC`);
+      roleAssign(newState);
+    } else if (newState.channelId === null) {
+      ch.send(`${userMention(oldState.id)} left the VC`);
+      roleAssign(oldState);
+    } else if (oldState.selfMute !== newState.selfMute) {
+      roleAssign(newState);
+    }
+  });
+};
+module.exports = vcUpdate;
