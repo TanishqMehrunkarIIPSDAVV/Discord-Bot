dbd80a3171db7a1252a7178fc3d3da624969b86b
TanishqMehrunkarIIPSDAVV <mehrunkart@gmail.com>
2026-01-24 22:30:40 +0530

    bella added

--- features/mentionReaction.js
+++ features/mentionReaction.js
@@ -1,7 +1,8 @@
 const path = require("node:path");
 const client = require(`${path.dirname(__dirname)}/index.js`);
 
-const TARGET_USER_ID = "779206329813696522";
+const TARGET_USER_ID_1 = "779206329813696522";
+const TARGET_USER_ID_2 = "1462895979052269890";
 
 const mentionReaction = () => {
   client.on("messageCreate", async (message) => {
@@ -16,16 +17,23 @@ const mentionReaction = () => {
         );
 
         // Ignore replies to the target user
-        if (repliedMessage.author.id === TARGET_USER_ID) return;
+        if (repliedMessage.author.id === TARGET_USER_ID_1 || repliedMessage.author.id === TARGET_USER_ID_2) return;
       } catch (err) {
         console.error("Failed to fetch replied message:", err);
       }
     }
 
     // React only if the user is directly mentioned (not via role)
-    if (message.mentions.users.has(TARGET_USER_ID)) {
+    if (message.mentions.users.has(TARGET_USER_ID_1)) {
       try {
         await message.react("🍭");
+      } catch (error) {
+        console.error("Error reacting to mention:", error);
+      }
+    }
+    else if (message.mentions.users.has(TARGET_USER_ID_2)) {
+      try {
+        await message.react("<:Bella_wink:1443660540667760660>");
       } catch (error) {
         console.error("Error reacting to mention:", error);
       }
